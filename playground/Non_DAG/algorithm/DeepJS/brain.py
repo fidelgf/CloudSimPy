96e5d6617c5007274360ac044f25e232c86c6500
fidelgf <38870318+fidelgf@users.noreply.github.com>
2021-06-20 12:06:50 +0200

    Update brain.py

--- playground/Non_DAG/algorithm/DeepJS/brain.py
+++ playground/Non_DAG/algorithm/DeepJS/brain.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+import numpy as np
 
 class BrainBig(tf.keras.Model):
     name = 'BrainBig'
@@ -28,30 +28,11 @@ class Brain(tf.keras.Model):
 
     def __init__(self, state_size):
         super().__init__()
-        self.dense_1 = tf.keras.layers.Dense(3, input_shape=(None, state_size), activation=tf.tanh)
-        self.dense_2 = tf.keras.layers.Dense(9, activation=tf.tanh)
-        self.dense_3 = tf.keras.layers.Dense(18, activation=tf.tanh)
-        self.dense_4 = tf.keras.layers.Dense(9, activation=tf.tanh)
-        self.dense_5 = tf.keras.layers.Dense(1)
-
-    def call(self, state):
-        state = self.dense_1(state)
-        state = self.dense_2(state)
-        state = self.dense_3(state)
-        state = self.dense_4(state)
-        state = self.dense_5(state)
-        return tf.expand_dims(tf.squeeze(state, axis=-1), axis=0)
-
-
-class BrainSmall(tf.keras.Model):
-    name = 'BrainSmall'
-
-    def __init__(self, state_size):
-        super().__init__()
         #self.dense_1 = tf.keras.layers.Dense(3, input_shape=(None, state_size), activation=tf.tanh)
         #self.dense_2 = tf.keras.layers.Dense(9, activation=tf.tanh)
-        #self.dense_3 = tf.keras.layers.Dense(6, activation=tf.tanh)
-        #self.dense_4 = tf.keras.layers.Dense(1)
+        #self.dense_3 = tf.keras.layers.Dense(18, activation=tf.tanh)
+        #self.dense_4 = tf.keras.layers.Dense(9, activation=tf.tanh)
+        #self.dense_5 = tf.keras.layers.Dense(1)
         self.denseltsm_1 = tf.keras.layers.LSTM(units=4, input_shape=(state_size,1)
         self.denseltsm_2 = tf.keras.layers.Dense(units=1)
 
@@ -60,9 +41,29 @@ class BrainSmall(tf.keras.Model):
         #state = self.dense_2(state)
         #state = self.dense_3(state)
         #state = self.dense_4(state)
+        #state = self.dense_5(state)
         state = np.reshape(state, (state.shape[0],state.shape[1], 1))
         state = self.denseltsm_1(state)
-        state = self.denseltsm_2(state)
+        state = self.denseltsm_2(state)                            
+        return tf.expand_dims(tf.squeeze(state, axis=-1), axis=0)
+
+
+class BrainSmall(tf.keras.Model):
+    name = 'BrainSmall'
+
+    def __init__(self, state_size):
+        super().__init__()
+        self.dense_1 = tf.keras.layers.Dense(3, input_shape=(None, state_size), activation=tf.tanh)
+        self.dense_2 = tf.keras.layers.Dense(9, activation=tf.tanh)
+        self.dense_3 = tf.keras.layers.Dense(6, activation=tf.tanh)
+        self.dense_4 = tf.keras.layers.Dense(1)
+
+
+    def call(self, state):
+        state = self.dense_1(state)
+        state = self.dense_2(state)
+        state = self.dense_3(state)
+        state = self.dense_4(state)
         return tf.expand_dims(tf.squeeze(state, axis=-1), axis=0)
 
 class LSTMBrain(tf.keras.Model):
